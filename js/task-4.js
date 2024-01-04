d847fc2cd1e3e810e883afe7f810f593db209f9d
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-01-04 20:47:25 +0200

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector("form.login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const userEmail = event.target.elements.email.value.trim();
+    const userPassword = event.target.elements.password.value.trim();
+    if (userEmail && userPassword) {
+        const userData = {
+            email: userEmail,
+            password: userPassword,
+        };
+        console.log(userData); 
+        form.reset();
+    } else {
+        alert("All form fields must be filled in")
+    }
+}
