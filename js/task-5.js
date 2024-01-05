59fab1094b5bb004ba83403b1505b8ade215e98a
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-01-05 20:07:15 +0200

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+function changeBodyColor() {
+    const newColor = getRandomHexColor();
+    document.body.style.backgroundColor = newColor;
+    document.querySelector("span.color").textContent = newColor;
+}
+
+document.querySelector("button.change-color").addEventListener("click", changeBodyColor);
