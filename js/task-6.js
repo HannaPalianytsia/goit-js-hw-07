3523940bf0b5cb834eee51ab250825dacd51f8db
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-01-08 21:29:11 +0200

    task 6 done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const inputCount = document.getElementById("controls").querySelector("input");
+const boxWithBoxes = document.querySelector("div#boxes");
+
+function createBoxes(amount) {
+  const arrayBoxesCreate = [];
+  for (let i = 0; i < amount; i++) {
+    arrayBoxesCreate.push('<div class="box"></div>')
+  }
+  boxWithBoxes.innerHTML = arrayBoxesCreate.join('');
+  const arrayBoxesStyles = document.querySelectorAll('div.box');
+  let boxWidth = 30;
+  for (let i = 0; i < amount; i++) {
+    arrayBoxesStyles[i].style.width = `${boxWidth}px`;
+    arrayBoxesStyles[i].style.height = `${boxWidth}px`;
+    arrayBoxesStyles[i].style.backgroundColor = getRandomHexColor();
+    boxWidth += 10;
+  }
+}
+function destroyBoxes() {
+  boxWithBoxes.innerHTML = '';
+}
+
+btnCreate.addEventListener("click", (event) => { 
+  const count = inputCount.value;
+  if (count >= 1 && count <= 100 && count % 1 === 0) {
+    createBoxes(count);
+    inputCount.value = '';
+  }
+  
+});
+btnDestroy.addEventListener("click", destroyBoxes);
