187da3e88133ac370938987448c2be1a4feb69f2
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-01-17 13:15:59 +0200

    update task-6

--- js/task-6.js
+++ js/task-6.js
@@ -6,11 +6,12 @@ function getRandomHexColor() {
 
 const btnCreate = document.querySelector("button[data-create]");
 const btnDestroy = document.querySelector("button[data-destroy]");
-const inputCount = document.getElementById("controls").querySelector("input");
+const inputCount = document.querySelector("#controls>input");
 const boxWithBoxes = document.querySelector("div#boxes");
 
 function createBoxes(amount) {
   destroyBoxes();
+  const allBoxes = [];
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
     let size = 30 + i * 10;
@@ -18,8 +19,9 @@ function createBoxes(amount) {
       `width: ${size}px; 
       height: ${size}px;
       background-color: ${getRandomHexColor()};`;
-    boxWithBoxes.appendChild(box);
+    allBoxes.push(box);
   }
+  boxWithBoxes.append(...allBoxes);
 }
 function destroyBoxes() {
   boxWithBoxes.innerHTML = "";
